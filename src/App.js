f98a654387cc581c7b224846f8b9c30271fd0f7d
Lewis Munday <lewisjohnmunday@gmail.com>
2023-02-04 20:40:50 +0000

    Added Coming home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ReactComponent as IconGithub } from './assets/icons/github.svg';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render = () => {
+    return (
+      <div className="card">
+        <div className="header">
+          <div className="logo">
+            <a href=".">VEZORO</a>
+          </div>
+          <div className="social">
+            <a href="https://github.com/vezoro" title="GitHub" target="_blank" rel="noopener noreferrer">
+              <IconGithub className="icon" />
+            </a>
+          </div>
+        </div>
+        <div className="content">
+          <div className="title-holder">
+            <h1>Get ready for the change.</h1>
+            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+          </div>
+          <a href="mailto:contact@vezoro.co.uk">
+            <div className="cta">Send us an email</div>
+          </a>
+        </div>
+        <div className="footer">
+          <span>Copyright <a className="underlined" href="https://github.com/vezoro" target="_blank" rel="noopener noreferrer">Vezoro</a> 2023 </span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
